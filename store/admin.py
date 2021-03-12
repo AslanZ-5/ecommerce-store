6fc0f59811599922ad6923d077413aa542f7349e
Aslan <asl.zurabov@gmail.com>
2021-03-12 16:20:39 +0300

    changed 3/12/2021

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {
+        'slug': ('name',)
+    }
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price', 'in_stock', 'created', 'updated']
+    list_filter = ['in_stock', 'is_active']
+    list_editable = ['price',
+                     'in_stock']  # will be displayed as form widgets on the change list page, allowing users to edit and save multiple rows at once.
+    prepopulated_fields = {
+        'slug': ('title',)
+        # The main use for this functionality is to automatically generate the value for SlugField fields from one or more other fields.
+    }
--- store/views.py
+++ store/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Category, Product
+
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
+
+
+def product_detail(request, slug):
+    item = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store/product_detail.html', {'item': item})
+
+
+def category_list(request,category_slug):
+    category_item = get_object_or_404(Category,slug=category_slug)
+    products = Product.objects.filter(category=category_item)
+
+    return render(request,'store/category_slug.html',{'products':products, 'category':category_item})
+
